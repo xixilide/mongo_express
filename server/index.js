35266155f12f7a49de18eff558479cfe89bf32c8
xixilide <754396775@qq.com>
2016-12-12 09:21:03 +0800

    xiu

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise;//解决异步操作出现的错误
 mongoose.connect('mongodb://localhost:27017/babel');
 //连接我的数据库　－－babel
 var User = require('./models/post');
@@ -12,16 +12,19 @@ var bodyParser = require('body-parser');
 // app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false })); // 如果想使用 form 提交，这一行是必要的
 var cors= require('cors');
-app.use(cors());
+app.use(cors());//使用中间件
 db.on('error', console.log);
 
-// db.once('open', function() {
-//   var post = new User({username: 'siman'});
-//  post.save(function(err){
-//    if(err) console.log(err);
-//  })
-//  console.log('success!');
-// });
+db.once('open', function() {
+ //  var post = new User({username:"xixilide",
+ //    password:"111",
+ //    email:"221www@sina.com",
+ //    age:"23"});
+ // post.save(function(err){
+ //   if(err) console.log(err);
+ // })
+ console.log('success!');
+});
 
 db.once('open', function() {
   User.find().exec(function(err, users) {
@@ -34,8 +37,25 @@ app.get('/users', function(req,res ){
     res.json({users})
   })
 })
+//可以在web API 中体现
 
+app.get('/users/:_id', function(req,res ){
+  User.findById(req.params._id,function (err,user) {
+     if(err) return console.log(err);
+     console.log(user);
+     res.send(user)
+})
+})
+//取得每个用户id来获取
+
+// app.get('/users/:username', function(req,res ){
+//   User.find({username:req.params.username},function (err,user) {
+//     if(err) return console.log(err);
+//       res.send({user})
+//   })
+// })
+//注意：/:username :不能省略
 app.listen(4000, function() {
   console.log('Express server is listening on port 4000')
 })
-//后台端口，不能与前台
+//后台端口，不能与前台相同
--- server/models/post.js
+++ server/models/post.js
@@ -1,14 +1,27 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-
+// var bcrypt = require('bcrypt');
 const UserSchema = new Schema(
   {
     username: { type: String },
-    password: { type: String }
+    password: { type: String },
+    email: { type:String },
+    age: { type:Number}
   },
   { timestamps: true }
 );
-
+// UserSchema.pre('save', function(next) {
+//   var user = this, SALT_FACTOR = 5;
+//   bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+//     if(err) return next(err);
+//     bcrypt.hash(user.password, salt, function(err, hash) {
+//       if (err) return next(err);
+//       user.password = hash;
+//       next();
+//     });
+//   });
+// });//密码保护
 module.exports = mongoose.model('User', UserSchema);
+//将Schema转化为model,判断映射到哪个集合，特别注意大小写
 //User　会自动对应数据库中的 users 这个集合
 //如：　Person  对应　people
